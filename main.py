d6a524b690d144362d1a7b80a96a62c2735afd33
fearthepink <stoffer.dylan@proton.me>
2025-11-14 00:32:58 -0600

    V: I did a thing

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
+
 from stats import (count_words, count_characters, sort_dict_list)
 
+
 def main():
-    
-    path = "books/frankenstein.txt"
+    import sys
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
+
+
+    path = sys.argv[1]
     book_text = get_book_text(path)
     count = count_words(book_text)
     count_characters(book_text)
